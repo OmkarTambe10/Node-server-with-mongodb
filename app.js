f587c9f954c6e6070df5aa6537ed62f1227e2cbe
OmkarTambe10 <70093818+OmkarTambe10@users.noreply.github.com>
2020-08-23 17:24:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,18 +5,6 @@ const mongoose = require("mongoose"); //NoSQL database
 var bodyParser = require('body-parser');
 var route = require("./routes/form.js");
 //connection of server on port no.9000
-app.listen( port, function(){
-    console.log("Connected to port " + port );
-});
-
-//connection of NoSQL database
-mongoose.connect('mongodb://127.0.0.1/dashboard', { useUnifiedTopology: true, useNewUrlParser: true});
-// mongoose.set('useUnifiedTopology', true);
-
-var db = mongoose.connection;
-db.once('open', function() {
-  console.log("database connected");
-});
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
@@ -24,5 +12,14 @@ app.set('view engine', 'ejs')
 app.use("/route/form", route);
 
 
-
+//connection of NoSQL database
+mongoose.connect('mongodb://127.0.0.1/dashboard', { useUnifiedTopology: true, useNewUrlParser: true, function(error, db){
+    if(error){
+        console.log(error);
+    }else{
+        app.listen( port, function(){
+            console.log("Connected to port " + port );
+        });    
+    }
+}});
 module.exports = app;
